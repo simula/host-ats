a1f461700e2821ab73f123c6dbd3b2bca2b86473
malekhammou <malekhammou2017@gmail.com>
2022-07-12 23:15:48 +0100

    added tests configuration file in test_suie.json

--- code/wrapper_script.py
+++ code/wrapper_script.py
@@ -1,4 +1,14 @@
 import os
-nfe_values=[50,75,100,150,175,200,225,265,300,350,400,450,500,550,650,700,850]
-for val in nfe_values:
- os.system("python3 create_thumbnail.py %s -nfe %d" % ('../data/videos/sample.mp4' ,val))
+import json
+file = open('../code/test_suite.json')
+data = json.load(file)
+for entry in data:
+ for val in range(50,entry["nfe"],entry["frame_extraction_step"]):
+  if entry['runStatic']:
+    os.system("python3 create_thumbnail.py %s -nfe %d -st" % ('../data/videos/' ,val))
+  elif entry['runMlDefault']:
+    os.system("python3 create_thumbnail.py %s -nfe %d" % ('../data/videos/' ,val))
+  else:
+    os.system("python3 create_thumbnail.py %s -nfe %d %s" % ('../data/videos/' ,val," ".join(entry["args"])))
+
+
